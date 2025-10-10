23565dabd1fe6b207b1acaaa1ab6b626d271f7df
Jason Gedge <jason@gedge.ca>
2025-10-10 11:38:36 -0400

    Have keys line up in json-viewer by setting margins

--- src/server/routes/__debug/GET-:url.ts
+++ src/server/routes/__debug/GET-:url.ts
@@ -47,33 +47,26 @@ export const debugPageGet: MateRoute = async ({ data: { site }, params: { url },
       json-viewer {
         margin-left: 1rem;
 
-        /* Background, font and indentation */
         --background-color: #ffffff;
         --color: #000000;
         --font-family: monospace;
         --font-size: 0.8rem;
         --line-height: 1rem;
 
-        --indent-size: 0.5em;
+        --indent-size: 0em;
         --indentguide-size: 1px;
-        --indentguide-style: solid;
-        --indentguide-color: #333;
-        --indentguide-color-active: #666;
-        --indentguide: var(--indentguide-size) var(--indentguide-style) var(--indentguide-color);
-        --indentguide-active: var(--indentguide-size) var(--indentguide-style) var(--indentguide-color-active);
+        --indentguide-style: dotted;
+        --indentguide-color: #888888;
 
         --outline-color: #e0e4e5;
         --outline-width: 1px;
         --outline-style: dotted;
 
-        /* Types colors */
         --string-color: #336622;
         --number-color: #112266;
         --boolean-color: #4ba7ef;
         --null-color: #441144;
         --property-color: #000000;
-
-        /* Collapsed node preview */
         --preview-color: #deae8f;
       }
     </style>
@@ -82,7 +75,15 @@ export const debugPageGet: MateRoute = async ({ data: { site }, params: { url },
       document.addEventListener("DOMContentLoaded", () => {
         // Ugh, styling custom components can be a pain :cry:
         const sheet = new CSSStyleSheet;
-        sheet.replaceSync(".preview { margin-left: 1ch }");
+        sheet.replaceSync(\`
+          .preview {
+            margin-left: 1ch;
+          }
+
+          .key:not(.collapsable) {
+            margin-left: var(--line-height);
+          }
+        \`);
         for (const host of document.querySelectorAll("json-viewer")) {
           host.shadowRoot.adoptedStyleSheets.push(sheet);
         }
