e651ccae81fab9a245cbbe0b90a9f33dbb3e0366
Jason Gedge <jason@gedge.ca>
2025-10-17 16:38:48 -0400

    Switch to vitest
    In my opinion, the node test runner is of poor design and not worth spending any
    more of my time on. Excessive default output, hard to read assertion messages,
    and actively works against the way I'd like to use it (e.g., try figuring out
    how to do a "run only this test" workflow).

--- test/utils/FileMatcher.test.ts
+++ test/utils/FileMatcher.test.ts
@@ -1,189 +1,192 @@
 import path from "node:path";
 import { Readable } from "node:stream";
-import { test } from "node:test";
+import { describe, expect, test } from "vitest";
 import { FileMatcher } from "../../src/utils/FileMatcher.js";
 
-test.assert.register("pathMatches", function (fileMatcher: FileMatcher, path: string) {
-  this.assert.equal(fileMatcher.matches(path), true, `${path} should match`);
+expect.extend({
+  toMatchPath(fileMatcher: FileMatcher, path: string) {
+    return {
+      pass: fileMatcher.matches(path),
+      message: () => `${path} should match`,
+    };
+  },
 });
 
-test.assert.register("notPathMatches", function (fileMatcher: FileMatcher, path: string) {
-  this.assert.equal(fileMatcher.matches(path), false, `${path} should not match`);
-});
-
-declare module "node:test" {
-  interface TestContextAssert {
-    pathMatches(fileMatcher: FileMatcher, path: string): void;
-    notPathMatches(fileMatcher: FileMatcher, path: string): void;
-  }
+interface CustomMatchers<R = unknown> {
+  toMatchPath: (path: string) => R;
 }
 
-test("FileMatcher", (t) => {
-  t.test("fromOptions", (t) => {
-    t.test("throws an error if given a relative base path", async (t) => {
-      await t.assert.rejects(() => FileMatcher.fromOptions({ base: "test" }));
+declare module "vitest" {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
+
+describe("FileMatcher", () => {
+  describe("fromOptions", () => {
+    test("throws an error if given a relative base path", async () => {
+      await expect(() => FileMatcher.fromOptions({ base: "test" })).rejects.toThrow("base must be an absolute path");
     });
 
-    t.test("with only globs", (t) => {
-      t.test("matches at any level for literal patterns without directory separators", async (t) => {
+    describe("with only globs", () => {
+      test("matches at any level for literal patterns without directory separators", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["*.csv"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "./file.csv");
-        t.assert.pathMatches(fileMatcher, "test/test.csv");
-        t.assert.pathMatches(fileMatcher, "a/b/c/test.csv");
-        t.assert.notPathMatches(fileMatcher, ".file.csv");
-        t.assert.notPathMatches(fileMatcher, "test.txt");
-        t.assert.notPathMatches(fileMatcher, "../file.csv");
+        expect(fileMatcher).toMatchPath("file.csv");
+        expect(fileMatcher).toMatchPath("./file.csv");
+        expect(fileMatcher).toMatchPath("test/test.csv");
+        expect(fileMatcher).toMatchPath("a/b/c/test.csv");
+        expect(fileMatcher).not.toMatchPath(".file.csv");
+        expect(fileMatcher).not.toMatchPath("test.txt");
+        expect(fileMatcher).not.toMatchPath("../file.csv");
       });
 
-      t.test("matches exactly at base with literal patterns starting with a directory separator", async (t) => {
+      test("matches exactly at base with literal patterns starting with a directory separator", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["/*.csv"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.notPathMatches(fileMatcher, "blah/file.csv");
-        t.assert.notPathMatches(fileMatcher, "nested/blah/test.csv");
+        expect(fileMatcher).toMatchPath("file.csv");
+        expect(fileMatcher).not.toMatchPath("blah/file.csv");
+        expect(fileMatcher).not.toMatchPath("nested/blah/test.csv");
       });
 
-      t.test("matches relative to base with literal patterns containing with a directory separator", async (t) => {
+      test("matches relative to base with literal patterns containing with a directory separator", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["blah/*.csv"],
         });
 
-        t.assert.notPathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "blah/file.csv");
-        t.assert.notPathMatches(fileMatcher, "nested/blah/test.csv");
+        expect(fileMatcher).not.toMatchPath("file.csv");
+        expect(fileMatcher).toMatchPath("blah/file.csv");
+        expect(fileMatcher).not.toMatchPath("nested/blah/test.csv");
       });
 
-      t.test("does not match if given includes and excludes and nothing matches", async (t) => {
+      test("does not match if given includes and excludes and nothing matches", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["*.csv", "!*.txt"],
         });
 
-        t.assert.notPathMatches(fileMatcher, "test.txt");
-        t.assert.notPathMatches(fileMatcher, "homer_bushes.gif");
+        expect(fileMatcher).not.toMatchPath("test.txt");
+        expect(fileMatcher).not.toMatchPath("homer_bushes.gif");
       });
 
-      t.test("matches dot files when given the dot option", async (t) => {
-        t.skip("`dot: true` option needs to be implemented");
+      test.skip("matches dot files when given the dot option", async () => {
+        // `dot: true` option needs to be implemented"
 
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["**/*.txt", "!test/file.js"],
           dot: true,
         });
-        t.assert.pathMatches(fileMatcher, ".file.txt");
-        t.assert.pathMatches(fileMatcher, "test/file.txt");
-        t.assert.pathMatches(fileMatcher, ".thing/test/file.txt");
-        t.assert.pathMatches(fileMatcher, "thing/.test/file.txt");
-        t.assert.pathMatches(fileMatcher, ".thing/.test/file.txt");
-        t.assert.pathMatches(fileMatcher, ".test/file.js");
-        t.assert.pathMatches(fileMatcher, "test/.file.js");
-        t.assert.pathMatches(fileMatcher, ".test/.file.js");
-        t.assert.notPathMatches(fileMatcher, "test/file.js");
+        expect(fileMatcher).toMatchPath(".file.txt");
+        expect(fileMatcher).toMatchPath("test/file.txt");
+        expect(fileMatcher).toMatchPath(".thing/test/file.txt");
+        expect(fileMatcher).toMatchPath("thing/.test/file.txt");
+        expect(fileMatcher).toMatchPath(".thing/.test/file.txt");
+        expect(fileMatcher).toMatchPath(".test/file.js");
+        expect(fileMatcher).toMatchPath("test/.file.js");
+        expect(fileMatcher).toMatchPath(".test/.file.js");
+        expect(fileMatcher).not.toMatchPath("test/file.js");
       });
 
-      t.test("excludes matches with an anchored ignore pattern", async (t) => {
+      test("excludes matches with an anchored ignore pattern", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["*.csv", "!/*.csv"],
         });
 
-        t.assert.notPathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "blah/file.csv");
-        t.assert.pathMatches(fileMatcher, "nested/blah/test.csv");
+        expect(fileMatcher).not.toMatchPath("file.csv");
+        expect(fileMatcher).toMatchPath("blah/file.csv");
+        expect(fileMatcher).toMatchPath("nested/blah/test.csv");
       });
 
-      t.test("matches non-ignored paths when only ignore globs are provided", async (t) => {
+      test("matches non-ignored paths when only ignore globs are provided", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["!*.csv", "!blah"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.txt");
-        t.assert.notPathMatches(fileMatcher, "blah/file.txt");
-        t.assert.notPathMatches(fileMatcher, "file.csv");
-        t.assert.notPathMatches(fileMatcher, "blah/file.csv");
-        t.assert.notPathMatches(fileMatcher, "nested/blah/test.csv");
+        expect(fileMatcher).toMatchPath("file.txt");
+        expect(fileMatcher).not.toMatchPath("blah/file.txt");
+        expect(fileMatcher).not.toMatchPath("file.csv");
+        expect(fileMatcher).not.toMatchPath("blah/file.csv");
+        expect(fileMatcher).not.toMatchPath("nested/blah/test.csv");
       });
 
-      t.test("does not match when given an ignore pattern", async (t) => {
+      test("does not match when given an ignore pattern", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["file.js", "!test/file.js"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.js");
-        t.assert.notPathMatches(fileMatcher, "test/file.js");
+        expect(fileMatcher).toMatchPath("file.js");
+        expect(fileMatcher).not.toMatchPath("test/file.js");
       });
 
-      t.test("always matches when given no patterns, unless outside of base path", async (t) => {
+      test("always matches when given no patterns, unless outside of base path", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["# This is a comment", ""],
           base: "/Users/jane",
         });
-        t.assert.pathMatches(fileMatcher, "/Users/jane/test/file.txt");
-        t.assert.notPathMatches(fileMatcher, "/Users/john/test/file.js");
+        expect(fileMatcher).toMatchPath("/Users/jane/test/file.txt");
+        expect(fileMatcher).not.toMatchPath("/Users/john/test/file.js");
       });
 
-      t.test("matches intermediate directories when given a ** pattern", async (t) => {
+      test("matches intermediate directories when given a ** pattern", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["blah/**/*.txt"],
         });
-        t.assert.pathMatches(fileMatcher, "blah/test/file.txt");
-        t.assert.pathMatches(fileMatcher, "blah/nested/test/file.txt");
-        t.assert.notPathMatches(fileMatcher, "noblah/test/file.txt");
+        expect(fileMatcher).toMatchPath("blah/test/file.txt");
+        expect(fileMatcher).toMatchPath("blah/nested/test/file.txt");
+        expect(fileMatcher).not.toMatchPath("noblah/test/file.txt");
       });
 
-      t.test("last pattern takes precedence when there is overlap", async (t) => {
+      test("last pattern takes precedence when there is overlap", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["test/**", "!test/**/*.txt", "test/blah/thing.txt"],
         });
-        t.assert.notPathMatches(fileMatcher, "test/file.txt");
-        t.assert.pathMatches(fileMatcher, "test/file.csv");
-        t.assert.notPathMatches(fileMatcher, "test/file/file.txt");
-        t.assert.notPathMatches(fileMatcher, "test/file/file2.txt");
-        t.assert.notPathMatches(fileMatcher, "test/file/file/file.txt");
-        t.assert.pathMatches(fileMatcher, "test/blah/thing.txt");
+        expect(fileMatcher).not.toMatchPath("test/file.txt");
+        expect(fileMatcher).toMatchPath("test/file.csv");
+        expect(fileMatcher).not.toMatchPath("test/file/file.txt");
+        expect(fileMatcher).not.toMatchPath("test/file/file2.txt");
+        expect(fileMatcher).not.toMatchPath("test/file/file/file.txt");
+        expect(fileMatcher).toMatchPath("test/blah/thing.txt");
       });
 
-      t.test("matches only absolute paths that start with the base when given the base option", async (t) => {
+      test("matches only absolute paths that start with the base when given the base option", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["*.txt"],
           base: "/Users/jane",
         });
-        t.assert.pathMatches(fileMatcher, "file.txt");
-        t.assert.pathMatches(fileMatcher, "/Users/jane/file.txt");
-        t.assert.notPathMatches(fileMatcher, "/Users/john/file.txt");
+        expect(fileMatcher).toMatchPath("file.txt");
+        expect(fileMatcher).toMatchPath("/Users/jane/file.txt");
+        expect(fileMatcher).not.toMatchPath("/Users/john/file.txt");
       });
 
-      t.test("matches exactly at base with literal patterns starting with a directory separator", async (t) => {
+      test("matches exactly at base with literal patterns starting with a directory separator", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           globs: ["/*.csv"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.notPathMatches(fileMatcher, "blah/file.csv");
-        t.assert.notPathMatches(fileMatcher, "nested/blah/test.csv");
+        expect(fileMatcher).toMatchPath("file.csv");
+        expect(fileMatcher).not.toMatchPath("blah/file.csv");
+        expect(fileMatcher).not.toMatchPath("nested/blah/test.csv");
       });
 
-      t.test("matches only directories when given a directory pattern", async (t) => {
+      test("matches only directories when given a directory pattern", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           base: path.join(import.meta.dirname, "../../"),
           globs: ["utils/"],
         });
 
-        t.assert.pathMatches(fileMatcher, "test/utils");
-        t.assert.pathMatches(fileMatcher, "test/utils/");
-        t.assert.pathMatches(fileMatcher, "utils/FileMatcher.test.ts");
-        t.assert.notPathMatches(fileMatcher, "utils");
-        t.assert.notPathMatches(fileMatcher, "utils/");
-        t.assert.notPathMatches(fileMatcher, "nested/blah/test.csv");
+        expect(fileMatcher).toMatchPath("test/utils");
+        expect(fileMatcher).toMatchPath("test/utils/");
+        expect(fileMatcher).toMatchPath("utils/FileMatcher.test.ts");
+        expect(fileMatcher).not.toMatchPath("utils");
+        expect(fileMatcher).not.toMatchPath("utils/");
+        expect(fileMatcher).not.toMatchPath("nested/blah/test.csv");
       });
     });
 
-    t.test("with only files", (t) => {
-      t.test("matches exactly at base with literal patterns starting with a directory separator", async (t) => {
+    describe("with only files", () => {
+      test("matches exactly at base with literal patterns starting with a directory separator", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           files: [
             stream(`
@@ -193,42 +196,41 @@ test("FileMatcher", (t) => {
           ],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "blah/file.csv");
-        t.assert.pathMatches(fileMatcher, "nested/blah/test.csv");
-        t.assert.notPathMatches(fileMatcher, "test.txt");
-        t.assert.notPathMatches(fileMatcher, "blah/test.txt");
+        expect(fileMatcher).toMatchPath("file.csv");
+        expect(fileMatcher).toMatchPath("blah/file.csv");
+        expect(fileMatcher).toMatchPath("nested/blah/test.csv");
+        expect(fileMatcher).not.toMatchPath("test.txt");
+        expect(fileMatcher).not.toMatchPath("blah/test.txt");
       });
 
-      t.test("ignores files that do not exist", async (t) => {
+      test("ignores files that do not exist", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           files: [stream(`*.csv`), "does_not_exist.ignore"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "blah/file.csv");
-        t.assert.pathMatches(fileMatcher, "nested/blah/test.csv");
-        t.assert.notPathMatches(fileMatcher, "test.txt");
-        t.assert.notPathMatches(fileMatcher, "blah/test.txt");
+        expect(fileMatcher).toMatchPath("file.csv");
+        expect(fileMatcher).toMatchPath("blah/file.csv");
+        expect(fileMatcher).toMatchPath("nested/blah/test.csv");
+        expect(fileMatcher).not.toMatchPath("test.txt");
+        expect(fileMatcher).not.toMatchPath("blah/test.txt");
       });
     });
 
-    t.test("with files and globs", (t) => {
-      t.test("matches exactly at base with literal patterns starting with a directory separator", async (t) => {
+    describe("with files and globs", () => {
+      test("matches exactly at base with literal patterns starting with a directory separator", async () => {
         const fileMatcher = await FileMatcher.fromOptions({
           files: [stream(`*.csv`)],
           globs: ["!blah.csv"],
         });
 
-        t.assert.pathMatches(fileMatcher, "file.csv");
-        t.assert.pathMatches(fileMatcher, "blah/test.csv");
-        t.assert.notPathMatches(fileMatcher, "blah.csv");
+        expect(fileMatcher).toMatchPath("file.csv");
+        expect(fileMatcher).toMatchPath("blah/test.csv");
+        expect(fileMatcher).not.toMatchPath("blah.csv");
       });
     });
 
-    t.test("withBase", (t) => {
-      t.test("creates a new FileMatcher with the same rules but a different base directory", async (t) => {
+    describe("withBase", () => {
+      test("creates a new FileMatcher with the same rules but a different base directory", async () => {
         const fileMatcher = (
           await FileMatcher.fromOptions({
             globs: ["*.txt"],
@@ -236,9 +238,9 @@ test("FileMatcher", (t) => {
           })
         ).withBase("/Users/john");
 
-        t.assert.pathMatches(fileMatcher, "file.txt");
-        t.assert.notPathMatches(fileMatcher, "/Users/jane/file.txt");
-        t.assert.pathMatches(fileMatcher, "/Users/john/file.txt");
+        expect(fileMatcher).toMatchPath("file.txt");
+        expect(fileMatcher).not.toMatchPath("/Users/jane/file.txt");
+        expect(fileMatcher).toMatchPath("/Users/john/file.txt");
       });
     });
   });
