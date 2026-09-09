dc74bb8bbb10dbce8fe895d1cf8cf1bbcd9177c7
Jason Gedge <jason@gedge.ca>
2026-09-09 12:31:11 -0400

    [utils] Improve `scalarCompare`
    Compares things like NaN and scalars of different types in a way that ensures
    a connected relation (see https://en.wikipedia.org/wiki/Connected_relation).

--- src/utils/scalarCompare.ts
+++ src/utils/scalarCompare.ts
@@ -14,28 +14,58 @@ export type Comparable =
 /**
  * Compare two scalars.
  *
- * @returns a negative number if `keyA < keyB`, a positive number if `keyA > keyB`, and 0 if `keyA = keyB`.
+ * @returns a negative number if `a < b`, a positive number if `a > b`, and 0 if `a == b`.
  */
-export const scalarCompare = (keyA: unknown, keyB: unknown): number => {
-  if (keyA === keyB) {
+export const scalarCompare = (a: unknown, b: unknown): number => {
+  if (a === b) {
     return 0;
   }
 
-  if (keyA === undefined) {
+  if (a == undefined) {
     return -1;
   }
 
-  if (keyB === undefined) {
+  if (b == undefined) {
     return 1;
   }
 
-  if (keyA === null) {
-    return -1;
+  if (isNumerical(a) && isNumerical(b)) {
+    const isNanA = Number.isNaN(a);
+    const isNanB = Number.isNaN(b);
+    if (isNanA && isNanB) {
+      return 0;
+    } else if (isNanA) {
+      return -1;
+    } else if (isNanB) {
+      return 1;
+    } else {
+      return a < b ? -1 : 1;
+    }
   }
 
-  if (keyB === null) {
-    return 1;
+  const typeA = typeof a;
+  const typeB = typeof b;
+  if (typeA != typeB) {
+    return TypeOrders[typeA] - TypeOrders[typeB];
   }
 
-  return keyA < keyB ? -1 : 1;
+  return a < b ? -1 : 1;
+};
+
+const isNumerical = (value: unknown): value is number | bigint => {
+  const t = typeof value;
+  return t == "number" || t == "bigint";
+};
+
+export const TypeOrders = {
+  string: 100,
+  number: 200,
+  bigint: 300,
+  symbol: 400,
+  boolean: 500,
+  null: 500,
+  undefined: 600,
+  array: 700,
+  object: 800,
+  function: 900,
 };
---
+++ test/utils/scalarCompare.test.ts
@@ -0,0 +1,32 @@
+import { inspect } from "node:util";
+import { describe, expect, test } from "vitest";
+
+import { scalarCompare } from "../../src/utils/scalarCompare.js";
+
+describe("scalarCompare", () => {
+  for (const [a, b] of [
+    [0, 1],
+    [123n, 555],
+    [undefined, 0],
+    [Number.NaN, 0],
+    ["value", 0],
+    ["a", "b"],
+    [false, true],
+    [null, ""],
+    ["", Number.POSITIVE_INFINITY],
+  ]) {
+    const presentA = inspect(a);
+    const presentB = inspect(b);
+    test(`returns a negative value when comparing ${presentA} to ${presentB}`, () => {
+      expect(scalarCompare(a, b)).toBeLessThan(0);
+    });
+
+    test(`returns a positive value when comparing ${presentB} to ${presentA}`, () => {
+      expect(scalarCompare(b, a)).toBeGreaterThan(0);
+    });
+
+    test(`returns zero when comparing ${presentA} to itself`, () => {
+      expect(scalarCompare(a, a)).toBe(0);
+    });
+  }
+});
